1fc68f591ad75d90fec53cd5ddbae0b66c390129
shifeng <a245241964@gmail.com>
2023-10-11 15:41:53 -0400

    implemented basic function of comment.

--- app.py
+++ app.py
@@ -11,22 +11,24 @@ def index():
     return 'comment server up and running'
 
 @app.route('/comment/<id>')
-
-
-def post(id):
-    posts = {
+def comment(id):
+    comments = {
         '1': {'user_id': '1', 'post_id' : '2', 'comment' : 'Amazing post!'},
         '2': {'user_id': '2', 'post_id' : '2', 'comment' : 'I did not know that'}
     }
-    post_info = posts.get(id, {})
+    comment_info = comments.get(id, {})
     
     # Get user info from User Service
-    if post_info:
-        response = requests.get(f'https://userservice.ashyrock-0da57b89.canadacentral.azurecontainerapps.io/user/{post_info["user_id"]}')
-        if response.status_code == 200:
-            post_info['user'] = response.json()
+    if comment_info:
 
-    return jsonify(post_info)
+
+        userResponse = requests.get(f'https://userservice.ashyrock-0da57b89.canadacentral.azurecontainerapps.io/user/{comment_info["user_id"]}')
+        postResponse = requests.get(f'https://postservice.ashyrock-0da57b89.canadacentral.azurecontainerapps.io/post/{comment_info["post_id"]}')
+        if userResponse.status_code and postResponse.status_code == 200:
+            comment_info['user'] = userResponse.json()
+            comment_info['post'] = postResponse.json()['post']
+
+    return jsonify(comment_info)
 
 if __name__ == '__main__':
     app.run()
