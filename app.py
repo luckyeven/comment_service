1d6d69065afc1da27ddfdec992d351763851efa3
shifeng <a245241964@gmail.com>
2023-10-11 16:07:26 -0400

    added CRUD operations

--- app.py
+++ app.py
@@ -2,9 +2,13 @@
 
 from flask import Flask, jsonify
 import requests
+comments_count = 2
+comments = {
+    '1': {'user_id': '1', 'post_id' : '2', 'comment' : 'Amazing post!'},
+    '2': {'user_id': '2', 'post_id' : '2', 'comment' : 'I did not know that'}
+}
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -12,10 +16,6 @@ def index():
 
 @app.route('/comment/<id>')
 def comment(id):
-    comments = {
-        '1': {'user_id': '1', 'post_id' : '2', 'comment' : 'Amazing post!'},
-        '2': {'user_id': '2', 'post_id' : '2', 'comment' : 'I did not know that'}
-    }
     comment_info = comments.get(id, {})
     
     # Get user info from User Service
@@ -29,6 +29,34 @@ def comment(id):
             comment_info['post'] = postResponse.json()['post']
 
     return jsonify(comment_info)
+
+@app.route('/create/<user_id>/<post_id>/<comment>')
+def create(user_id,post_id,comment):
+    global comments_count
+    comment = {
+        'user_id': user_id,
+        'post_id': post_id,
+        'comment': comment
+    }
+    comments_count += 1
+    comments[str(comments_count)] = comment
+    return jsonify(comment)
+
+@app.route('/delete/<id>')
+def delete(id):
+    if comments.get(id):
+        del comments[id]
+        return 'Comment deleted'
+    else:
+        return 'Comment not found'
+    
+@app.route('/update/<id>/<comment>')
+def update(id, comment):
+    if comments.get(id):
+        comments[id]['comment'] = comment
+        return 'Comment updated'
+    else:
+        return 'Comment not found'
 
 if __name__ == '__main__':
     app.run()
